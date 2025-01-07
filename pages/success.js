03054d10119fd1a2bd894d1a643b723ff3af1568
MohamedH1000 <mohammedhisham115@gmail.com>
2025-01-07 20:46:40 +0200

    adjust the webhook in stripe

--- pages/success.js
+++ pages/success.js
@@ -1,8 +1,12 @@
+import Layout from "../components/Layout/Layout";
+
 const Success = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1>Payment Successful!</h1>
-    </div>
+    <Layout>
+      <div className="min-h-screen flex justify-center items-center">
+        <h1>Payment Successful!</h1>
+      </div>
+    </Layout>
   );
 };
 
